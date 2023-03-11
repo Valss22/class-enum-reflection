94c044dfb0bef95a724c9d9e4ce4c861341997e3
Valss22 <valsshokorov@gmail.com>
2023-03-11 22:11:57 +0600

    mapper enum to class added

--- main.php
+++ main.php
@@ -1,27 +1,33 @@
 <?php
 
+$classToEnumMapper = [
+    TestClass::class => TestEnum::class
+];
+
 enum TestEnum : int 
 {
     case one = 1;
     case two = 2;
 }
 
-$reflection = new ReflectionClass(TestEnum::class);
-$constants = $reflection->getConstants();
+// class TestClass
+// {
+//     public $one = 1;
+//     public $two = 2;
+// }
 
-foreach ($constants as $name => $value) {
-    $constants["$name"] = $value->value;
+// $props = get_object_vars(new TestClass);
+
+foreach ($classToEnumMapper as $class => $enum)
+{
+    $reflection = new ReflectionClass($enum);
+    $constants = $reflection->getConstants();
+
+    foreach ($constants as $name => $value)
+    {
+        $constants["$name"] = $value->value;
+    }
+    echo var_dump($constants);
 }
-echo var_dump($constants);
 
-
-class TestClass {
-    public $one = 1;
-    public $two = 2;
-}
-
-$props = get_object_vars(new TestClass());
-echo var_dump($props);
-
-echo $constants === $props;
 ?>
